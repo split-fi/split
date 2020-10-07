8db21bcd712bcde93e4f2a1e3c40753bedb2214a
Francesco Agosti <francesco.agosti93@gmail.com>
2020-10-06 21:39:08 -0700

    Add buidler files with typescript support

---
+++ buidler.config.ts
@@ -0,0 +1,21 @@
+import { task, usePlugin, BuidlerConfig } from "@nomiclabs/buidler/config";
+
+usePlugin("@nomiclabs/buidler-waffle");
+
+// This is a sample Buidler task. To learn how to create your own go to
+// https://buidler.dev/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, bre) => {
+  const accounts = await bre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(await account.getAddress());
+  }
+});
+
+const config: BuidlerConfig = {
+  solc: {
+    version: "0.6.8",
+  },
+}
+
+export default config;
