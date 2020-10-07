6c265c70559ccd84ef79dce604c4ca0511050812
Francesco Agosti <francesco.agosti93@gmail.com>
2020-10-06 21:53:15 -0700

    Add typechain

--- buidler.config.ts
+++ buidler.config.ts
@@ -1,6 +1,7 @@
 import { task, usePlugin, BuidlerConfig } from "@nomiclabs/buidler/config";
 
 usePlugin("@nomiclabs/buidler-waffle");
+usePlugin("buidler-typechain")
 
 // This is a sample Buidler task. To learn how to create your own go to
 // https://buidler.dev/guides/create-task.html
@@ -15,6 +16,10 @@ task("accounts", "Prints the list of accounts", async (taskArgs, bre) => {
 const config: BuidlerConfig = {
   solc: {
     version: "0.6.8",
+  },
+  typechain: {
+    outDir: "typechain",
+    target: "ethers-v5",
   },
 }
 
--- test/greeter.ts
+++ test/greeter.ts
@@ -1,10 +1,12 @@
 import { expect } from "chai";
 import { ethers } from "@nomiclabs/buidler";
 
+import { Greeter } from "../typechain/Greeter";
+
 describe("Greeter", function() {
   it("Should return the new greeting once it's changed", async function() {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
+    const GreeterFactory = await ethers.getContractFactory("GreeterFactory");
+    const greeter = (await GreeterFactory.deploy("Hello, world!")) as Greeter;
     
     await greeter.deployed();
     expect(await greeter.greet()).to.equal("Hello, world!");
