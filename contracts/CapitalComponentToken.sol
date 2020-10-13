b3388dc5adb8997f9456415c906595e7e0f2b06f
Francesco Agosti <francesco.agosti93@gmail.com>
2020-10-13 12:13:22 -0700

    Implement payout in the capital component token

--- contracts/CapitalComponentToken.sol
+++ contracts/CapitalComponentToken.sol
@@ -7,7 +7,9 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
 
 import "./interfaces/PriceOracle.sol";
+import "./SplitVault.sol";
 import "./lib/PriceUtils.sol";
+import "./lib/DSMath.sol";
 
 contract CapitalComponentToken is ERC20, Ownable {
   using SafeMath for uint256;
@@ -15,14 +17,17 @@ contract CapitalComponentToken is ERC20, Ownable {
   address public fullToken;
   uint8 private fullTokenDecimals;
   PriceOracle private priceOracle;
+  SplitVault private splitVault;
 
   constructor(
     string memory name,
     string memory symbol,
     address _fullToken,
-    address priceOracleAddress
+    address priceOracleAddress,
+    address splitVaultAddress
   ) public ERC20(name, symbol) {
     priceOracle = PriceOracle(priceOracleAddress);
+    splitVault = SplitVault(splitVaultAddress);
     fullToken = _fullToken;
     // Make sure the fullToken has implemented the decimals method before allowing init.
     fullTokenDecimals = ERC20(fullToken).decimals();
@@ -49,6 +54,31 @@ contract CapitalComponentToken is ERC20, Ownable {
   /// @param amount amount of tokens to burn
   function burn(address account, uint256 amount) public onlyOwner {
     _burn(account, amount);
-    // TODO(fragosti): tell vault to return the corresponding amount of full tokens.
+    uint256 payoutAmount = calculatePayoutAmount(amount);
+    // Call the payout function on the SplitVault contract
+    splitVault.payout(payoutAmount, fullToken, account);
+  }
+
+  /// @dev Simplest public method for calculating the amount of fullToken due for a given amount of capital token
+  /// @param capitalTokenAmount Amount of capital token to calculate the payout from
+  /// @return The payout amount denoted in fullToken
+  function calculatePayoutAmount(uint256 capitalTokenAmount) public view returns (uint256) {
+    uint256 currPrice = priceOracle.getPrice(fullToken);
+    return calculatePayoutAmount(capitalTokenAmount, currPrice, fullTokenDecimals);
+  }
+
+  /// @dev Pure function for calculating the amount of fullToken due for a given amount of capital token
+  /// @param capitalTokenAmount Amount of capital token to calculate the payout from
+  /// @param currPrice The current price of the fullToken with respect to the underlying
+  /// @param tokenDecimals The decimal precision of the fullTokenn
+  /// @return The payout amount denoted in fullToken
+  function calculatePayoutAmount(
+    uint256 capitalTokenAmount,
+    uint256 currPrice,
+    uint8 tokenDecimals
+  ) public pure returns (uint256) {
+    uint256 fullTokenPayoutWads = DSMath.wdiv(capitalTokenAmount, currPrice);
+    uint256 payoutAmount = PriceUtils.fromWadToDecimals(fullTokenPayoutWads, tokenDecimals);
+    return payoutAmount;
   }
 }
