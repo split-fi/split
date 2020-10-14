f39f5bc771d8f6b9cda6c1f379acd4b88ae52fc0
Francesco Agosti <francesco.agosti93@gmail.com>
2020-10-13 18:32:49 -0700

    Refactor SplitVault tests to use real component tokens

--- test/split_vault.ts
+++ test/split_vault.ts
@@ -2,53 +2,90 @@ import { expect, use } from "chai";
 import { ethers } from "@nomiclabs/buidler";
 import { solidity } from "ethereum-waffle";
 
+import { PriceOracleMock } from "../typechain/PriceOracleMock";
+import { CTokenMock } from "../typechain/CTokenMock";
 import { SplitVault } from "../typechain/SplitVault";
 
 import { ACCOUNT_1, NULL_ADDRESS } from "./constants";
 import { ComponentTokenDependencyAddresses } from "./types";
+import { getDeployedCapitalComponentToken, getDeployedYieldComponentToken } from "./utils";
 
-// TODO(fabio): Update these with actual values once tokens have been added to deployer
-const TOKEN_ADDRESS = "0x4a77faee9650b09849ff459ea1476eab01606c7a";
-const YIELD_TOKEN_ADDRESS = "0xb3f7fB482492f4220833De6D6bfCC81157214bEC";
-const CAPITAL_TOKEN_ADDRESS = "0x41B5844f4680a8C38fBb695b7F9CFd1F64474a72";
+const ERC20_DECIMALS = 8;
 
 use(solidity);
 
+export interface SplitVaultDependencyAddresses {
+  fullTokenAddress: string;
+  oracleAddress: string;
+}
+
+const getDeployedContracts = async (addresses: SplitVaultDependencyAddresses) => {
+  const SplitVaultFactory = await ethers.getContractFactory("SplitVault");
+  const splitVault = (await SplitVaultFactory.deploy()) as SplitVault;
+  await splitVault.deployed();
+  const deployedAddresses: ComponentTokenDependencyAddresses = {
+    ...addresses,
+    splitVaultAddress: splitVault.address,
+  };
+  const name = "X Token";
+  const symbol = "XXX";
+  const yieldComponentToken = await getDeployedYieldComponentToken(name, symbol, deployedAddresses);
+  const capitalComponentToken = await getDeployedCapitalComponentToken(name, symbol, deployedAddresses);
+  return {
+    splitVault,
+    yieldComponentToken,
+    capitalComponentToken,
+  };
+};
+
 describe("SplitVault", function () {
+  let erc20Token: CTokenMock;
+  let priceOracle: PriceOracleMock;
+  let addresses: SplitVaultDependencyAddresses;
+
+  before(async () => {
+    const PriceOracleMockFactory = await ethers.getContractFactory("PriceOracleMock");
+    priceOracle = (await PriceOracleMockFactory.deploy()) as PriceOracleMock;
+    await priceOracle.deployed();
+
+    const CTokenMockFactory = await ethers.getContractFactory("CTokenMock");
+    erc20Token = (await CTokenMockFactory.deploy("A Token", "AAA", ERC20_DECIMALS)) as CTokenMock;
+    await erc20Token.deployed();
+
+    addresses = {
+      fullTokenAddress: erc20Token.address,
+      oracleAddress: priceOracle.address,
+    };
+  });
   describe("add function", function () {
     it("should add componentSet when called by owner", async function () {
-      const SplitVault = await ethers.getContractFactory("SplitVault");
-      const splitVault = (await SplitVault.deploy()) as SplitVault;
-
-      await splitVault.deployed();
-      await splitVault.add(TOKEN_ADDRESS, YIELD_TOKEN_ADDRESS, CAPITAL_TOKEN_ADDRESS);
-
-      expect(await splitVault.getComponentSet(TOKEN_ADDRESS)).to.be.deep.equal([
-        YIELD_TOKEN_ADDRESS,
-        CAPITAL_TOKEN_ADDRESS,
+      const { splitVault, yieldComponentToken, capitalComponentToken } = await getDeployedContracts(addresses);
+      await splitVault.add(addresses.fullTokenAddress, yieldComponentToken.address, capitalComponentToken.address);
+      expect(await splitVault.getComponentSet(addresses.fullTokenAddress)).to.be.deep.equal([
+        yieldComponentToken.address,
+        capitalComponentToken.address,
       ]);
     });
     it("should not add componentSet when called by non-owner", async function () {
-      const SplitVault = await ethers.getContractFactory("SplitVault");
-      let splitVault = (await SplitVault.deploy()) as SplitVault;
-
-      await splitVault.deployed();
+      const { splitVault, yieldComponentToken, capitalComponentToken } = await getDeployedContracts(addresses);
       const signers = await ethers.getSigners();
       const nonOwner = signers[1];
 
       await expect(
-        splitVault.connect(nonOwner).add(TOKEN_ADDRESS, YIELD_TOKEN_ADDRESS, CAPITAL_TOKEN_ADDRESS),
+        splitVault
+          .connect(nonOwner)
+          .add(addresses.fullTokenAddress, yieldComponentToken.address, capitalComponentToken.address),
       ).to.be.revertedWith("Ownable: caller is not the owner");
 
-      expect(await splitVault.getComponentSet(TOKEN_ADDRESS)).to.be.deep.equal([NULL_ADDRESS, NULL_ADDRESS]);
+      expect(await splitVault.getComponentSet(addresses.fullTokenAddress)).to.be.deep.equal([
+        NULL_ADDRESS,
+        NULL_ADDRESS,
+      ]);
     });
   });
   describe("split", function () {
     it("should revert if user attempts to split an unregistered token", async function () {
-      const SplitVault = await ethers.getContractFactory("SplitVault");
-      const splitVault = (await SplitVault.deploy()) as SplitVault;
-
-      await splitVault.deployed();
+      const { splitVault } = await getDeployedContracts(addresses);
       const unregisteredTokenAddress = NULL_ADDRESS;
       const amount = 10;
       await expect(splitVault.split(amount, unregisteredTokenAddress)).to.be.revertedWith(
@@ -58,10 +95,7 @@ describe("SplitVault", function () {
   });
   describe("combine", function () {
     it("should revert if user attempts to recombine an unregistered token", async function () {
-      const SplitVault = await ethers.getContractFactory("SplitVault");
-      const splitVault = (await SplitVault.deploy()) as SplitVault;
-
-      await splitVault.deployed();
+      const { splitVault } = await getDeployedContracts(addresses);
       const unregisteredTokenAddress = NULL_ADDRESS;
       const amount = 10;
       await expect(splitVault.combine(amount, unregisteredTokenAddress)).to.be.revertedWith(
@@ -71,37 +105,28 @@ describe("SplitVault", function () {
   });
   describe("payout", function () {
     it("should revert if user attempts to call payout for an unregistered token", async function () {
-      const SplitVault = await ethers.getContractFactory("SplitVault");
-      const splitVault = (await SplitVault.deploy()) as SplitVault;
-
-      await splitVault.deployed();
+      const { splitVault } = await getDeployedContracts(addresses);
       const recipient = ACCOUNT_1;
       const amount = 10;
-      await expect(splitVault.payout(amount, TOKEN_ADDRESS, recipient)).to.be.revertedWith(
+      await expect(splitVault.payout(amount, addresses.fullTokenAddress, recipient)).to.be.revertedWith(
         "Attempted to request a payout for an unsupported token",
       );
     });
     it("should revert if msg.sender isn't the corresponding YieldERC20Token", async function () {
-      const SplitVault = await ethers.getContractFactory("SplitVault");
-      const splitVault = (await SplitVault.deploy()) as SplitVault;
-
-      await splitVault.deployed();
-      // Add a CToken to the contract, so we don't revert because the token is unregistered
-      await splitVault.add(TOKEN_ADDRESS, YIELD_TOKEN_ADDRESS, CAPITAL_TOKEN_ADDRESS);
+      const { splitVault, yieldComponentToken, capitalComponentToken } = await getDeployedContracts(addresses);
+      await splitVault.add(addresses.fullTokenAddress, yieldComponentToken.address, capitalComponentToken.address);
 
       const recipient = ACCOUNT_1;
       const amount = 10;
-      await expect(splitVault.payout(amount, TOKEN_ADDRESS, recipient)).to.be.revertedWith(
+      await expect(splitVault.payout(amount, addresses.fullTokenAddress, recipient)).to.be.revertedWith(
         "Payout can only be called by the corresponding yield or capital token",
       );
     });
   });
   describe("getComponentSet", function () {
     it("should return a zero-value component set for an unregistered token", async function () {
-      const SplitVault = await ethers.getContractFactory("SplitVault");
-      const splitVault = (await SplitVault.deploy()) as SplitVault;
-
-      await splitVault.deployed();
+      const { splitVault, yieldComponentToken, capitalComponentToken } = await getDeployedContracts(addresses);
+      await splitVault.add(addresses.fullTokenAddress, yieldComponentToken.address, capitalComponentToken.address);
       const unregisteredTokenAddress = NULL_ADDRESS;
       expect(await splitVault.getComponentSet(unregisteredTokenAddress)).to.be.deep.equal([NULL_ADDRESS, NULL_ADDRESS]);
     });
