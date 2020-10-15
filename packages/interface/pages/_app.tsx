e1316c3d2f0fb60c673400ec7f0b7e020cf2dd02
David Sun <me@bydavidsun.com>
2020-10-14 21:38:42 -0500

    ADD dependencies

--- packages/interface/pages/_app.tsx
+++ packages/interface/pages/_app.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import App from 'next/app';
 import styled from 'styled-components';
+import { Web3ReactProvider } from '@web3-react/core';
+import { Web3Provider } from '@ethersproject/providers';
 import { ThemedGlobalStyle } from '../theme';
 
 const AppWrapper = styled.div`
 
 `;
+
+const getLibrary = (provider: any) => {
+    const library = new Web3Provider(provider);
+    library.pollingInterval = 15000;
+    return library;
+};
 
 export default class SplitApp extends App {
   render() {
@@ -17,9 +25,11 @@ export default class SplitApp extends App {
       <>
         <ThemedGlobalStyle/>
         <AppWrapper>
-            <Component
-            {...modifiedPageProps}
-            />
+            <Web3ReactProvider getLibrary={getLibrary}>
+                <Component
+                {...modifiedPageProps}
+                />
+            </Web3ReactProvider>
         </AppWrapper>
       </>
     );
