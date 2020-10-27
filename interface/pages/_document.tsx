8fd9ffa44a98cf49f52e3ba8412abe4d2113c98f
Francesco Agosti <francesco@fragosti.com>
2020-10-26 19:11:34 -0700

    Add GA (#50)
    * Add GA
    
    * Run prettier

--- interface/pages/_document.tsx
+++ interface/pages/_document.tsx
@@ -40,6 +40,16 @@ export default class SplitDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,700;1,400;1,700&display=swap"
             rel="stylesheet"
           />
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-4FHE20LTYG"></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-4FHE20LTYG');`,
+            }}
+          />
         </Head>
         <body>
           <Main />
